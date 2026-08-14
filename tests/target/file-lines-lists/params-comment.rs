f31ca2f1a1c2dbfb17eb2305a3fc1c625c37a7b8
Nicole LeGare <legaren@immunant.com>
2026-08-14 16:30:51 -0700

    Move tests into a directory

--- tests/target/file-lines-lists/params-comment.rs
+++ tests/target/file-lines-lists/params-comment.rs
@@ -1,4 +1,4 @@
-// rustfmt-file_lines: [{"file":"tests/source/file-lines-list-comment.rs","range":[7,7]}]
+// rustfmt-file_lines: [{"file":"tests/source/file-lines-lists/params-comment.rs","range":[7,7]}]
 
 fn foo(
 first    :    i32,
