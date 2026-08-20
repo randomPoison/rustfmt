594ec061489c1dfcdf0b2b56a46336a175250376
Nicole LeGare <legaren@immunant.com>
2026-08-20 13:43:03 -0700

    Preserve pre-comment when not selected

---
+++ tests/target/file-lines-lists/params-comment-before-selected.rs
@@ -0,0 +1,8 @@
+// rustfmt-file_lines: [{"file":"tests/source/file-lines-lists/params-comment-before-selected.rs","range":[6,6]}]
+
+fn foo(
+first    :    i32,
+// preserve this comment
+    second: i32,
+    selected    :    i32,
+) {}
