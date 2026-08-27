5feef80120fcaffbd9caaf2a271b26fbba4f2cd0
Nicole LeGare <legaren@immunant.com>
2026-08-27 13:03:05 -0700

    Add test for selecting first comment without selecting its item

---
+++ tests/source/file-lines-lists/fn-decl/params-first-comment-only.rs
@@ -0,0 +1,9 @@
+// rustfmt-file_lines: [{"file":"tests/source/file-lines-lists/fn-decl/params-first-comment-only.rs","range":[4,4]}]
+
+fn foo(
+// comment on first item
+first    :    i32,
+// preserve this comment
+        second    :    i32,
+    selected    :    i32,
+) {}
