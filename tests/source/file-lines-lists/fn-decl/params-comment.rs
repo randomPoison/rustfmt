e9d718f1f1b559edbe09544859fe141a00535f76
Nicole LeGare <legaren@immunant.com>
2026-08-27 12:28:29 -0700

    params-comment -> params-comment-unselected

--- tests/source/file-lines-lists/fn-decl/params-comment.rs
+++
@@ -1,8 +0,0 @@
-// rustfmt-file_lines: [{"file":"tests/source/file-lines-lists/fn-decl/params-comment.rs","range":[7,7]}]
-
-fn foo(
-first    :    i32,
-// preserve this comment
-        second    :    i32,
-    selected    :    i32,
-) {}
