d6d12a84c6c345403cab800409e4d149e40fb862
Nicole LeGare <legaren@immunant.com>
2026-08-28 15:12:55 -0700

    Add test for selecting the open paren and first arg

---
+++ tests/source/file-lines-lists/fn-decl/params-open-paren-selected.rs
@@ -0,0 +1,8 @@
+// rustfmt-file_lines: [{"file":"tests/source/file-lines-lists/fn-decl/params-open-paren-selected.rs","range":[3,4]}]
+
+fn foo(  
+first: u8,  
+second: bool,  
+third: char,  
+fourth: bool, fifth: u64) {
+}
