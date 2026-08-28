d768ef96c18a04cd8d8ad8f8f2872f45ecc9d18a
Nicole LeGare <legaren@immunant.com>
2026-08-28 15:06:48 -0700

    Add test for forcing vertical layout

---
+++ tests/target/file-lines-lists/fn-decl/params-full-horizontal.rs
@@ -0,0 +1,10 @@
+// rustfmt-file_lines: [{"file":"tests/source/file-lines-lists/fn-decl/params-full-horizontal.rs","range":[7,15]}]
+
+// Tests that we force a vertical layout if the open and close parens weren't
+// selected, even if the args would normally want to be laid out horizontally.
+
+fn foo(  
+    first: i32,
+    selected: i32,
+    last: i32
+) {}
