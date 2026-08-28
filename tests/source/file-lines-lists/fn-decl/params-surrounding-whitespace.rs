4eb1b7638733cda4f19d03ad8913c8badde86d08
Nicole LeGare <legaren@immunant.com>
2026-08-28 14:39:58 -0700

    Flesh out test for unselected whitespace

---
+++ tests/source/file-lines-lists/fn-decl/params-surrounding-whitespace.rs
@@ -0,0 +1,13 @@
+// rustfmt-file_lines: [{"file":"tests/source/file-lines-lists/fn-decl/params-surrounding-whitespace.rs","range":[8,8]}]
+
+fn foo(  
+first    :    i32,  
+    
+    
+       
+    selected     :  i32  ,  
+      
+        
+        last    :    i32,  
+   
+) {}
