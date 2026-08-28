b4aa7321fd9d2a91d387d071325008d7780300e0
Nicole LeGare <legaren@immunant.com>
2026-08-28 14:46:31 -0700

    Add test for selecting everything between two args

---
+++ tests/target/file-lines-lists/fn-decl/params-whitespace-between-selected.rs
@@ -0,0 +1,11 @@
+// rustfmt-file_lines: [{"file":"tests/source/file-lines-lists/fn-decl/params-whitespace-between-selected.rs","range":[8,11]}]
+
+fn foo(  
+first    :    i32,  
+    
+    
+       
+    selected: i32,
+    last: i32,
+   
+) {}
